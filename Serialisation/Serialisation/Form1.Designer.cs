d2453b04acb8befd019cf36425ebda65fad66673
Vincent <vincentnaef@gmail.com>
2017-02-10 15:27:48 +0100

    Serialisation

---
+++ Serialisation/Serialisation/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Serialisation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Serialization = new System.Windows.Forms.Button();
+            this.tbx_Name = new System.Windows.Forms.TextBox();
+            this.tbx_Score = new System.Windows.Forms.TextBox();
+            this.lbl_Name = new System.Windows.Forms.Label();
+            this.lbl_Score = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Serialization
+            // 
+            this.btn_Serialization.Location = new System.Drawing.Point(229, 234);
+            this.btn_Serialization.Name = "btn_Serialization";
+            this.btn_Serialization.Size = new System.Drawing.Size(75, 23);
+            this.btn_Serialization.TabIndex = 0;
+            this.btn_Serialization.Text = "Sérialisation";
+            this.btn_Serialization.UseVisualStyleBackColor = true;
+            this.btn_Serialization.Click += new System.EventHandler(this.btn_Serialization_Click);
+            // 
+            // tbx_Name
+            // 
+            this.tbx_Name.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbx_Name.Location = new System.Drawing.Point(242, 44);
+            this.tbx_Name.Name = "tbx_Name";
+            this.tbx_Name.Size = new System.Drawing.Size(100, 20);
+            this.tbx_Name.TabIndex = 1;
+            // 
+            // tbx_Score
+            // 
+            this.tbx_Score.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbx_Score.Location = new System.Drawing.Point(242, 88);
+            this.tbx_Score.Name = "tbx_Score";
+            this.tbx_Score.Size = new System.Drawing.Size(100, 20);
+            this.tbx_Score.TabIndex = 2;
+            // 
+            // lbl_Name
+            // 
+            this.lbl_Name.AutoSize = true;
+            this.lbl_Name.Location = new System.Drawing.Point(201, 47);
+            this.lbl_Name.Name = "lbl_Name";
+            this.lbl_Name.Size = new System.Drawing.Size(35, 13);
+            this.lbl_Name.TabIndex = 3;
+            this.lbl_Name.Text = "Nom :";
+            // 
+            // lbl_Score
+            // 
+            this.lbl_Score.AutoSize = true;
+            this.lbl_Score.Location = new System.Drawing.Point(195, 91);
+            this.lbl_Score.Name = "lbl_Score";
+            this.lbl_Score.Size = new System.Drawing.Size(41, 13);
+            this.lbl_Score.TabIndex = 4;
+            this.lbl_Score.Text = "Score :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 292);
+            this.Controls.Add(this.lbl_Score);
+            this.Controls.Add(this.lbl_Name);
+            this.Controls.Add(this.tbx_Score);
+            this.Controls.Add(this.tbx_Name);
+            this.Controls.Add(this.btn_Serialization);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Serialization;
+        private System.Windows.Forms.TextBox tbx_Name;
+        private System.Windows.Forms.TextBox tbx_Score;
+        private System.Windows.Forms.Label lbl_Name;
+        private System.Windows.Forms.Label lbl_Score;
+    }
+}
+
---
+++ Serialisation/Serialisation/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace Serialisation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Serialization_Click(object sender, EventArgs e)
+        {
+            //System.IO.StreamWriter file = new System.IO.StreamWriter("C:\\Users\\NAEFV_INFO\\Desktop\\a.txt", true);
+            //file.WriteLine("");
+            //file.Close();
+
+            //MD5 first = MD5.Create();
+            
+            //MD5 second = MD5.Create();
+            
+            //if(firstHash == secondHash)
+            //{
+            //    lbl_Name.Text = "ZER";
+            //}
+            StringBuilder hash = new StringBuilder();
+            MD5CryptoServiceProvider md5provider = new MD5CryptoServiceProvider();
+            byte[] bytes = md5provider.ComputeHash(new UTF8Encoding().GetBytes("Vincent"));
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hash.Append(bytes[i].ToString());
+            }
+
+            
+
+            StringBuilder hash2 = new StringBuilder();
+            MD5CryptoServiceProvider md5provider2 = new MD5CryptoServiceProvider();
+            byte[] bytes2 = md5provider.ComputeHash(new UTF8Encoding().GetBytes("Vincent"));
+
+            if(bytes == bytes2)
+            {
+                lbl_Name.Text = "AAA";
+            }
+        }
+
+    }
+}
